dd167d8162734e4e00949089825240f24d126127
quin288 <quynhmeo288@gmail.com>
2024-07-24 17:15:03 +0700

    footer

--- view/footer.php
+++ view/footer.php
@@ -25,115 +25,115 @@
 
 <!-- Start Footer Section -->
 <footer class="footer-section">
-    <div class="container relative">
+			<div class="container relative">
 
-        <!-- <div class="sofa-img">
-            <img src="./view/asset/images/sofa.png" alt="Image" class="img-fluid">
-        </div> -->
-
-        <div class="row">
-            <div class="col-lg-8">
-                <div class="subscription-form">
-                    <h3 class="d-flex align-items-center"><span class="me-1"><img src="./view/asset/images/envelope-outline.svg" alt="Image" class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
-
-                    <form action="#" class="row g-3">
-                        <div class="col-auto">
-                            <input type="text" class="form-control" placeholder="Enter your name">
-                        </div>
-                        <div class="col-auto">
-                            <input type="email" class="form-control" placeholder="Enter your email">
-                        </div>
-                        <div class="col-auto">
-                            <button class="btn btn-primary">
-                                <span class="fa fa-paper-plane"></span>
-                            </button>
-                        </div>
-                    </form>
-
-                </div>
-            </div>
-        </div>
-
-        <div class="row g-5 mb-5">
-            <div class="col-lg-4">
-                <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a></div>
-                <p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant</p>
-
-                <ul class="list-unstyled custom-social">
-                    <li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
-                    <li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
-                    <li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
-                    <li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
-                </ul>
-            </div>
-
-            <div class="col-lg-8">
-                <div class="row links-wrap">
-                    <div class="col-6 col-sm-6 col-md-3">
-                        <ul class="list-unstyled">
-                            <li><a href="#">About us</a></li>
-                            <li><a href="#">Services</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Contact us</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-6 col-sm-6 col-md-3">
-                        <ul class="list-unstyled">
-                            <li><a href="#">Support</a></li>
-                            <li><a href="#">Knowledge base</a></li>
-                            <li><a href="#">Live chat</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-6 col-sm-6 col-md-3">
-                        <ul class="list-unstyled">
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Our team</a></li>
-                            <li><a href="#">Leadership</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-6 col-sm-6 col-md-3">
-                        <ul class="list-unstyled">
-                            <li><a href="#">Nordic Chair</a></li>
-                            <li><a href="#">Kruzo Aero</a></li>
-                            <li><a href="#">Ergonomic Chair</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="border-top copyright">
-            <div class="row pt-4">
-                <div class="col-lg-6">
-                    <p class="mb-2 text-center text-lg-start">Copyright &copy;<script>
-                            document.write(new Date().getFullYear());
-                        </script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a> Distributed By <a hreff="https://themewagon.com">ThemeWagon</a> <!-- License information: https://untree.co/license/ -->
-                    </p>
+				<div class="sofa-img">
+                <img src="./theme/bia3.png" alt="Image" class="img-fluid">				
                 </div>
 
-                <div class="col-lg-6 text-center text-lg-end">
-                    <ul class="list-unstyled d-inline-flex ms-auto">
-                        <li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                    </ul>
-                </div>
+				<div class="row">
+					<div class="col-lg-8">
+						<div class="subscription-form">
+						<h3 class="d-flex align-items-center"><span class="me-1"><img src="./theme/furni-1.0.0/images/envelope-outline.svg" 
+						alt="Image" class="img-fluid"></span><span>Đăng ký</span></h3>
+						
+						<form action="#" class="row g-3">
+								<div class="col-auto">
+									<input type="text" class="form-control" placeholder="Nhập tên của bạn">
+								</div>
+								<div class="col-auto">
+									<input type="email" class="form-control" placeholder="Nhập email của bạn">
+								</div>
+								<div class="col-auto">
+									<button class="btn btn-primary">
+										<span class="fa fa-paper-plane"></span>
+									</button>
+								</div>
+							</form>
 
-            </div>
-        </div>
+						</div>
+					</div>
+				</div>
 
-    </div>
-</footer>
-<!-- End Footer Section -->
+				<div class="row g-5 mb-5">
+					<div class="col-lg-4">
+						<div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo"><img src="./theme/logohq.PNG"></a></div>
+						<p class="mb-4">© 2017 Công Ty Cổ Phần Vàng Bạc Đá HQ 
+							<br>170E Phan Đăng Lưu, P.3, Q.Nam Từ Liêm, TP.Hà Nội
+							<br>ĐT: 028 39951703 - Fax: 028 3995 1702
+							<br>Giấy chứng nhận đăng ký doanh nghiệp: 0300521758 ,do Sở Kế hoạch & Đầu tư TP.HN cấp lần đầu ngày 02/01/2024</p>
+
+						<ul class="list-unstyled custom-social">
+							<li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
+							<li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
+							<li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
+							<li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
+						</ul>
+					</div>
+
+					<div class="col-lg-8">
+						<div class="row links-wrap">
+							<div class="col-6 col-sm-6 col-md-3">
+								<ul class="list-unstyled">
+									<li><a href="#">Về HQ</a></li>
+									<li><a href="#">Tuyển dụng</a></li>
+									<li><a href="#">Kiểm định kim cương</a></li>
+									<li><a href="#">Kinh doanh sỉ</a></li>
+								</ul>
+							</div>
+
+							<div class="col-6 col-sm-6 col-md-3">
+								<ul class="list-unstyled">
+									<li><a href="#">Dịch vụ khách hàng</a></li>
+									<li><a href="#">Chính sách hoàn tiền</a></li>
+									<li><a href="#">Chính sách bảo hành thu đổi</a></li>
+									<li><a href="#">Cẩm nang sử dụng trang sức</a></li>
+								</ul>
+							</div>
+
+							<div class="col-6 col-sm-6 col-md-3">
+								<ul class="list-unstyled">
+									<li><a href="#">Dịch vụ giao hàng</a></li>
+									<li><a href="#">Hướng dẫn mua hàng</a></li>
+									<li><a href="#">Giao hàng tiết kiệm</a></li>
+								</ul>
+							</div>
+
+							<div class="col-6 col-sm-6 col-md-3">
+								<ul class="list-unstyled">
+									<li><a href="#">Hệ thống cửa hàng</a></li>
+									<li><a href="#">Cừa hàng toàn quốc</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="border-top copyright">
+					<div class="row pt-4">
+						<div class="col-lg-6">
+							<p class="mb-2 text-center text-lg-start">Copyright &copy;<script>document.write(new Date().getFullYear());</script><!--. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a> Distributed By <a hreff="https://themewagon.com">ThemeWagon</a>  <!-- License information: https://untree.co/license/ --></p>
+						</div>
+
+						<div class="col-lg-6 text-center text-lg-end">
+							<ul class="list-unstyled d-inline-flex ms-auto">
+								<li class="me-4"><a href="#">Điều khoản &amp; Điều kiện</a></li>
+								<li><a href="#">Chính sách bảo mật</a></li>
+							</ul>
+						</div>
+
+					</div>
+				</div>
+
+			</div>
+		</footer>
+		<!-- End Footer Section -->	
 
 
-<script src="./view/asset/js/bootstrap.bundle.min.js"></script>
-<script src="./view/asset/js/tiny-slider.js"></script>
-<script src="./view/asset/js/custom.js"></script>
+		<script src="./theme/furni-1.0.0/js/bootstrap.bundle.min.js"></script>
+		<script src="./theme/furni-1.0.0/js/tiny-slider.js"></script>
+		<script src="./theme/furni-1.0.0/js/custom.js"></script>
 </body>
 
 </html>
